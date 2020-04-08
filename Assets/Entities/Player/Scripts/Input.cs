b85b6acf25cc97b1f19f8f9cf2b2d217a94d934b
Duarte Oliveira <duartefmoliveira8@gmail.com>
2020-04-08 20:54:03 +0100

    feature: setup new input system

---
+++ Assets/Entities/Player/Scripts/Input.cs
@@ -0,0 +1,231 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Entities/Player/Input/Input.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Input : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Input()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Input"",
+    ""maps"": [
+        {
+            ""name"": ""Player2D"",
+            ""id"": ""fca18afe-a535-4bc0-a7a6-5bb26e97fba0"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""93243d4d-ceee-4f60-bb79-98fd5f6d7242"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""2cc47a97-963b-4677-a404-56967e35459d"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""7b381611-191b-4685-80f5-0261f5fe1b70"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c4fcc5d0-83ae-4f1b-828c-90a8973c8a35"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""2f839fbd-1941-4229-a48a-19df999a7fc0"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""03fd37fd-b4b1-4b2f-a187-9fae626544c6"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""62222d8b-5827-4e17-8a35-940b67e00568"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8910c91b-255b-47cd-a86a-e5bd8b0090f9"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard and mouse"",
+            ""bindingGroup"": ""Keyboard and mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Player2D
+        m_Player2D = asset.FindActionMap("Player2D", throwIfNotFound: true);
+        m_Player2D_Movement = m_Player2D.FindAction("Movement", throwIfNotFound: true);
+        m_Player2D_Jump = m_Player2D.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player2D
+    private readonly InputActionMap m_Player2D;
+    private IPlayer2DActions m_Player2DActionsCallbackInterface;
+    private readonly InputAction m_Player2D_Movement;
+    private readonly InputAction m_Player2D_Jump;
+    public struct Player2DActions
+    {
+        private @Input m_Wrapper;
+        public Player2DActions(@Input wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player2D_Movement;
+        public InputAction @Jump => m_Wrapper.m_Player2D_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Player2D; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(Player2DActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayer2DActions instance)
+        {
+            if (m_Wrapper.m_Player2DActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_Player2DActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_Player2DActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_Player2DActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_Player2DActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_Player2DActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_Player2DActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_Player2DActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public Player2DActions @Player2D => new Player2DActions(this);
+    private int m_KeyboardandmouseSchemeIndex = -1;
+    public InputControlScheme KeyboardandmouseScheme
+    {
+        get
+        {
+            if (m_KeyboardandmouseSchemeIndex == -1) m_KeyboardandmouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard and mouse");
+            return asset.controlSchemes[m_KeyboardandmouseSchemeIndex];
+        }
+    }
+    public interface IPlayer2DActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Entities/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private Input controls;
+    private Vector2 movementInput;
+
+    private void Awake() {
+        controls = new Input();
+        controls.Player2D.Movement.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+        controls.Player2D.Jump.performed += _ => Jump();
+    }
+
+    void Update() {
+        Debug.Log("Move: " + movementInput);
+    }
+
+    private void Jump() {
+        Debug.Log("Jump!");
+    }
+
+    void OnEnable() {
+        controls.Enable();
+    }
+
+    void OnDisable() {
+        controls.Disable();
+    }
+}
