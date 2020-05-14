20c836359124667c641fc2cfc7e74f08adb3e1f6
Antero Santos <up201604602@fe.up.pt>
2020-05-14 16:21:27 +0100

    add animatior controller, collider and script

---
+++ Assets/Entities/Interactables/Objects/Models/jump mushroom/JumpMushroom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpMushroom : MonoBehaviour {
+
+    private Animator animator;
+
+    private void Start() {
+        animator = gameObject.GetComponent<Animator>();   
+    }
+
+    void OnTriggerEnter(Collider other) {
+        animator.SetTrigger("activated");
+        //other.gameObject.GetComponent<Rigidbody>().AddForce(100*transform.up,ForceMode.VelocityChange);
+        other.gameObject.transform.position += 300 * Vector3.up * Time.deltaTime;
+    }
+}
