f22571af906d5147476f1e4807589c80c5f650e4
Mariana Filipa Costa <up201604414@fe.up.pt>
2020-05-24 17:25:10 +0100

    Start interaction with NPCs

---
+++ Assets/Entities/Interactables/NPC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public Dictionary<string, int> itemsNeeded;
+
+    public void Interact(Player player)
+    {
+        int itemCount = 0;
+        foreach (InventorySlot item in player.GetInventory().container)
+        {
+            int actualValue;
+            if (itemsNeeded.TryGetValue(item.item.name, out actualValue) && actualValue == item.amount)
+            {
+                itemCount++;
+            }
+        }
+        if (itemCount == itemsNeeded.Count)
+        {
+            // Player has all the item, remove them from the inventory and do stuff
+        }
+    }
+}
