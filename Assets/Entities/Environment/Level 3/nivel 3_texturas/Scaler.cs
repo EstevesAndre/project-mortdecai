848e1cc87fe1ed53b51e089f6a04fd578395cb45
Antero Santos <up201604602@fe.up.pt>
2020-06-08 17:01:58 +0100

    add moving water prefab

---
+++ Assets/Entities/Environment/Level 3/nivel 3_texturas/Scaler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour {
+
+private float mSize = 0.0f;
+private float inc = 1.0f;
+    void Start() {
+        InvokeRepeating("ScaleUp",0.0f,0.1f);
+    }
+
+    void ScaleUp() {
+        if(mSize >= 100.0f) {
+            CancelInvoke("ScaleUp");
+            InvokeRepeating("ScaleDown",0.0f,0.1f);
+        }
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(0,mSize++);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(1,mSize++);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(2,mSize++);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(3,mSize++);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(4,mSize++);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(5,mSize++);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(6,mSize++);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(7,mSize++);
+    }
+    void ScaleDown() {
+        if(mSize <= 0.0f) {
+            CancelInvoke("ScaleDown");
+            InvokeRepeating("ScaleUp",0.0f,0.1f);
+        }
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(0,mSize--);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(1,mSize--);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(2,mSize--);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(3,mSize--);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(4,mSize--);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(5,mSize--);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(6,mSize--);
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(7,mSize--);
+    }
+
+}
